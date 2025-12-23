e4d92e2f7bcbfd59690d1a28d73725a9c439aad0
Desirre Barbosa <desblssbarbosa@gmail.com>
2025-12-23 13:50:58 +0800

    honestly just like c but simplified

---
+++ string.py
@@ -0,0 +1,12 @@
+# can use single qoutes/ double
+Name = "Michael Jackson"
+print(Name[0:5])
+
+# can use negative indexing
+print(Name[-5:-1])
+
+# can skip
+print(Name[::2])
+
+# Slicing every second value inside index 5
+print(Name[0:5:2])
