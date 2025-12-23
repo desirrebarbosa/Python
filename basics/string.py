ebbbe6454db1d1017bdfb2648fb8d230154aa41f
Desirre Barbosa <desblssbarbosa@gmail.com>
2025-12-23 13:58:57 +0800

    regex!!

--- basics/string.py
+++ basics/string.py
@@ -10,3 +10,18 @@ print(Name[::2])
 
 # Slicing every second value inside index 5
 print(Name[0:5:2])
+
+# String manipulation
+a = 'Thriller is the sixth studio album'
+print("before upper: ", a)
+print("after upper:", a.upper())
+
+# reg ex: tool for matching and handling strings
+import re
+s1 = "The BodyGaurd is the best album"
+pattern = r"Body"
+result = re.search(pattern, s1)
+if result:
+    print("Match found!")
+else:
+    print("Match not found")
