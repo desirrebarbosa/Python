51a200ed155b88f05cda80761a1a6db5f1413e7c
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-01-06 17:53:46 +0800

    learned about condition and branching

---
+++ programming-fundamentals/condition-branching.py
@@ -0,0 +1,13 @@
+age = 17
+if age > 18:
+    print("you can enter")
+# elif instead of else if
+elif age == 18:
+    print("you are ", age)
+else:
+    print("go see meatloaf")
+print("move on")
+
+album_year = 1980
+if(album_year > 1979) and (album_year < 1981):
+    print("Album year issdlvnsdlnjdsv")
