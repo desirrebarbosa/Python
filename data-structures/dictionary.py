ddc314c51574b3a49f439e5c6f62c4dee429466f
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-01-06 16:50:40 +0800

    learned about dictionaries

---
+++ data-structures/dictionary.py
@@ -0,0 +1,10 @@
+# key value
+# list vs dictionary: list indexed by number, 
+# dictionary: labeled index
+
+Dict = {"key1" : 1, "key2" : 2, "key3" : [3, 3, 3],
+        "key4" : (4, 4, 4, 4), "key5" : 5, (0,1) : 6}
+print(Dict)
+
+# access dict
+print(Dict["key1"])
---
+++ data-structures/sets.py
@@ -0,0 +1,5 @@
+# are type of a collection, unordered
+# behaves just like mathematical sets
+
+set1 = {"pop", "rock", "soul", "hard rock"}
+print(set1)
