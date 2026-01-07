51ab3709b6e82bd631993d7de97e1d9338b1dde5
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-01-07 15:42:45 +0800

    learned about functions

---
+++ programming-fundamentals/functions.py
@@ -0,0 +1,18 @@
+def calculate_total(a, b):
+    total = a + b
+    return total
+
+result = calculate_total(10, 10)
+print(result)
+
+def square(a):
+    b = a * a
+    return b
+
+print(square(3))
+
+def divide(a, b):
+    div = a / b
+    return div
+result = divide(5, 2)
+print(result)
