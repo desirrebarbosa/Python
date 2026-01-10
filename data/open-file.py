8f9fc70889c1d1c09ca1a50ef97079d84371fb00
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-01-10 17:15:25 +0800

    added comments

--- data/open-file.py
+++ data/open-file.py
@@ -9,3 +9,7 @@ with open('file.txt', 'w') as file1:
     file1.write("This is line B\n")
 
 print_content('file.txt')
+
+# r+ is reading and writing, w+ is writing and reading, truncates the file
+# a+ is appending and reading. creates new file.
+
